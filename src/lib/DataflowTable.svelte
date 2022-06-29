e090b486baa7225926067455b197eb290195575a
radonnachie <code@radonn.co.za>
2022-06-29 21:22:36 +0200

    *]  DataflowTable rework

--- src/lib/DataflowTable.svelte
+++ src/lib/DataflowTable.svelte
@@ -11,13 +11,13 @@
     if (bytes < 1e3) {
       return `${bytes} Bytes`;
     } else if (bytes < 1e6) {
-      return `${(round_decimals(bytes, 6)/1e3).toFixed(3)} KBytes`;
+      return `${(round_decimals(bytes, 3)/1e3).toFixed(3)} KBytes`;
     } else if (bytes < 1e9) {
-      return `${(round_decimals(bytes, 6)/1e6).toFixed(6)} MBytes`;
+      return `${(round_decimals(bytes, 3)/1e6).toFixed(3)} MBytes`;
     } else if (bytes < 1e12) {
-      return `${(round_decimals(bytes, 6)/1e9).toFixed(6)} GBytes`;
+      return `${(round_decimals(bytes, 3)/1e9).toFixed(3)} GBytes`;
     } else if (bytes < 1e16) {
-      return `${(round_decimals(bytes, 6)/1e12).toFixed(6)} TBytes`;
+      return `${(round_decimals(bytes, 3)/1e12).toFixed(3)} TBytes`;
     }
   }
 </script>
@@ -31,13 +31,13 @@
     Stage
   </div>
   <div style="grid-column: 3;">
-    Ingest Bytes
+    Egress Bytes
   </div>
   <div style="grid-column: 4;">
     (ASPECTS, CHANNELS, TIMESAMPLES, POLARIZATIONS, DATATYPE)
   </div>
   <div style="grid-column: 5;">
-    I/O Ratio
+    I/O Ratio (Relative)
   </div>
 
   {#each dataflows as flow, i}
@@ -47,19 +47,24 @@
     <div style="grid-column: 2;">
       {flow.label}
     </div>
-    <div style="grid-column: 3;">
-      {byte_string(flow.datadim_out.bytesize())} ({flow.direction.to})
+    <div style="grid-column: 3;" class={flow.direction.to}>
+      {byte_string(flow.datadim_out.bytesize())}
+      {#if i > 0}
+        (x{round_decimals(flow.datadim_out.bytesize()/dataflows[i-1].datadim_out.bytesize(), 6)})
+      {/if}
     </div>
     <div style="grid-column: 4;">
       ({flow.datadim_out.aspects},
         {flow.datadim_out.channels},
         {flow.datadim_out.timesamples},
         {flow.datadim_out.polarizations},
-        {flow.datadim_out.datatype.label}
-      )
+        {flow.datadim_out.datatype.label})
     </div>
     <div style="grid-column: 5;">
       @ {flow.rate}
+      {#if i > 0}
+        (x{flow.rate/dataflows[i-1].rate})
+      {/if}
     </div>
   {/each}
 </div>
@@ -70,5 +75,11 @@
     display: grid;
     row-gap: 5px;
     margin-bottom: 10px;
+  }
+  div.CPU {
+    color: coral;
+  }
+  div.GPU {
+    color: teal;
   }
 </style>
