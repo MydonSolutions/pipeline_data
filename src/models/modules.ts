bd67bdcd89d7afa08b7aa45874534f0aeb0f1bd9
radonnachie <code@radonn.co.za>
2022-06-29 07:52:56 +0200

    ++ modules toJSON, _fromObject

--- src/models/modules.ts
+++ src/models/modules.ts
@@ -1,14 +1,14 @@
-import type { DataType } from "./datatypes";
+import { DataType, DataType_fromObject } from "./datatypes";
 import type { DataDimension } from "./datadimensions";
 export type {
   IModule,
 }
 export {
-  Accumulate,
-  Beamform,
-  Cast,
-  Channelize,
-  Detect,
+  Accumulate, Accumulate_fromObject,
+  Beamform, Beamform_fromObject,
+  Cast, Cast_fromObject,
+  Channelize, Channelize_fromObject,
+  Detect, Detect_fromObject,
 }
 
 interface IModule {
@@ -32,6 +32,16 @@ class Accumulate implements IModule{
   }
 
   /**
+   * toJSON
+   */
+  public toJSON() {
+    return {
+      "module": "Accumulate",
+      "length": this.length
+    }
+  }
+
+  /**
   * ingest: reduce the number of timesamples
   */
   public ingest(datadim:DataDimension):DataDimension {    
@@ -39,6 +49,16 @@ class Accumulate implements IModule{
     outdim.timesamples /= this.length;
     return outdim;
   }
+}
+function Accumulate_fromObject(jso:Object) {
+  [
+    'length',
+  ].forEach(prop => {
+    if(!jso.hasOwnProperty(prop)) {
+      throw new Error(`Accumulate from JSObject: Missing '${prop}' property (${jso}).`);
+    }
+  });
+  return new Accumulate(jso['length'])
 }
 
 class Beamform implements IModule{
@@ -58,6 +78,16 @@ class Beamform implements IModule{
   }
 
   /**
+   * toJSON
+   */
+  public toJSON() {
+    return {
+      "module": "Beamform",
+      "beams": this.beams
+    }
+  }
+
+  /**
   * ingest: change the number of sources (from antenna to beams)
   */
   public ingest(datadim:DataDimension):DataDimension {    
@@ -65,6 +95,16 @@ class Beamform implements IModule{
     outdim.aspects = this.beams;
     return outdim;
   }
+}
+function Beamform_fromObject(jso:Object) {
+  [
+    'beams',
+  ].forEach(prop => {
+    if(!jso.hasOwnProperty(prop)) {
+      throw new Error(`Beamform from JSObject: Missing '${prop}' property (${jso}).`);
+    }
+  });
+  return new Beamform(jso['beams'])
 }
 
 class Cast implements IModule{
@@ -84,6 +124,16 @@ class Cast implements IModule{
   }
 
   /**
+   * toJSON
+   */
+  public toJSON() {
+    return {
+      "module": "Cast",
+      "datatype": this.datatype
+    }
+  }
+
+  /**
   * ingest: change the DataDimension.datatype
   */
   public ingest(datadim:DataDimension):DataDimension {
@@ -91,6 +141,16 @@ class Cast implements IModule{
     outdim.datatype = this.datatype;
     return outdim;
   }
+}
+function Cast_fromObject(jso:Object) {
+  [
+    'datatype',
+  ].forEach(prop => {
+    if(!jso.hasOwnProperty(prop)) {
+      throw new Error(`Cast from JSObject: Missing '${prop}' property (${jso}).`);
+    }
+  });
+  return new Cast(DataType_fromObject(jso['datatype']))
 }
 
 class Channelize implements IModule{
@@ -110,6 +170,16 @@ class Channelize implements IModule{
   }
 
   /**
+   * toJSON
+   */
+  public toJSON() {
+    return {
+      "module": "Channelize",
+      "rate": this.rate
+    }
+  }
+
+  /**
   * ingest: upchannelize by a given rate
   */
   public ingest(datadim:DataDimension):DataDimension {
@@ -124,6 +194,16 @@ class Channelize implements IModule{
     outdim.channels *= this.rate;
     return outdim;
   }
+}
+function Channelize_fromObject(jso:Object) {
+  [
+    'rate',
+  ].forEach(prop => {
+    if(!jso.hasOwnProperty(prop)) {
+      throw new Error(`Channelize from JSObject: Missing '${prop}' property (${jso}).`);
+    }
+  });
+  return new Channelize(jso['rate'])
 }
 
 class Detect implements IModule{
@@ -143,6 +223,16 @@ class Detect implements IModule{
   }
 
   /**
+   * toJSON
+   */
+  public toJSON() {
+    return {
+      "module": "Detect",
+      "components": this.components
+    }
+  }
+
+  /**
   * ingest: collapse the polarisations
   */
   public ingest(datadim:DataDimension):DataDimension {    
@@ -150,4 +240,14 @@ class Detect implements IModule{
     outdim.polarizations = this.components;
     return outdim;
   }
+}
+function Detect_fromObject(jso:Object) {
+  [
+    'components',
+  ].forEach(prop => {
+    if(!jso.hasOwnProperty(prop)) {
+      throw new Error(`Detect from JSObject: Missing '${prop}' property (${jso}).`);
+    }
+  });
+  return new Detect(jso['components'])
 }
